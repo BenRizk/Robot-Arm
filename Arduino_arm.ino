66f7754d9685aec90e5e69eca87770dcb2a1cbb1
BenRizk <coolguyben@hotmail.com>
2017-05-23 19:32:32 -0400

    Added functions for color sensor
    Has yet to be tested.

--- Arduino_arm.ino
+++ Arduino_arm.ino
@@ -27,13 +27,25 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
+  // needed for color sensor
+  // we need to assign s0, s1, s2, s3, and sensorout to digital port
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(sensorOut, INPUT);
   
+  // Setting frequency-scaling to 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+
+  Serial.begin(9600);
 }
 
 void loop() {
 //beginning position
-liftservo.write(80);
-reachservo.write(50);
+liftservo.write(85);  // 85 o.g
+reachservo.write(40);  // 40 o.g
 
 sweep(pos, trigPin, echoPin);
 
@@ -42,6 +54,9 @@ sweep(pos, trigPin, echoPin);
 void sweep(int pos,int trigPin, int echoPin)
 {
    int distance;
+   int redValue;
+   int blueValue;
+   int greenValue;
    for (pos = 180; pos >= 140; pos--) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     baseservo.write(pos);              // tell servo to go to position in variable 'pos'
@@ -52,10 +67,36 @@ void sweep(int pos,int trigPin, int echoPin)
     if(distance > 7 && distance < 20)
     {
       reachOut(trigPin,echoPin);
+      delay(40);
       reachservo.write(50);
       delay(30);
       liftservo.write(80);
       delay(30);
+      // begin process of organizing by color
+      redValue = findRed();
+      blueValue = findBlue();
+      greenValue = findGreen();
+      //object is red when red value is least
+      if(redValue < blueValue && redValue < greenValue)
+      {
+        moveRed();
+      }
+      //object is blue if blue value is least
+      if(blueValue < redValue && blueValue < greenValue)
+      {
+        moveblue();
+      }
+      // object is green if green value is least
+      if(greenValue < blueValue && greenValue < redValue)
+      {
+        movegreen();
+      }
+      //object is black if all values are greater than 100
+      if(redValue > 100 && blueValue > 100 && greenValue > 100)
+      {
+        //not made yet
+      }
+    
     }
   }
   for (pos = 139; pos <= 180; pos++) { // goes from 180 degrees to 0 degrees
@@ -67,6 +108,7 @@ void sweep(int pos,int trigPin, int echoPin)
    if(distance > 7 && distance < 20)
     {
       reachOut(trigPin,echoPin);
+      delay(40);
       reachservo.write(50);
       delay(30);
       liftservo.write(80);
@@ -117,7 +159,7 @@ void reachOut(int trigPin,int echoPin)
         delay(30);
       }
       delay(30);
-    if(a <= 3)
+    if(a <= 3)  //end for loop when the head is close enough
     {
       b = i; // hold the last angle the arm was at
       i = 111; //for the loop to end
@@ -139,47 +181,106 @@ void reachOut(int trigPin,int echoPin)
     delay(30);
   }
    
-//trying to make the arm stop before it reaches its object
-/*
-int a = 0;
-int distance; 
-   
-  do{
-    distance = distanceSensor(trigPin, echoPin);
-    Serial.print("a:");
-    Serial.print(a);
-    a++;
-    reachservo.write(a);
-    delay(300);
-  }while(distance < 4);
-  clawservo.write(180); //close claw
-    delay(300);
-     for(int i = a; i > 35; i--)
-   {
-    reachservo.write(i);
-    delay(30);
-  }
-*/
-
 }
 void moveBlue()
 {
-  liftservo.write(30);
+  reachservo.write(130); //raise the arm high to avoid hitting objects
+  delay(40);
+  liftservo.write(60); // raise the lift servo
   delay(30);
-  baseservo.write(130);
+  baseservo.write(70); // move to blue zone
+  delay(30);
+  reachservo.write(90); // lower to far distance
+  delay(30);
+  liftservo.write(100); //lower the lift servo
+  delay(30);
+  clawservo.write(0); //open claw
+  delay(30);
+  reachservo.write(130); //raise the arm high to avoid hitting objects
+  delay(40);
+  liftservo.write(60); // raise the lift servo
   delay(30);
 }
 void moveRed()
 {
-  liftservo.write(30);
+  reachservo.write(130); // extend reach servo
+  delay(40);
+  liftservo.write(60); // raise lift servo
   delay(30);
-  baseservo.write(175);
+  baseservo.write(40); // move to red zone
+  delay(30);
+  reachservo.write(90); // lower to far distance
+  delay(30);
+  liftservo.write(100); //lower the lift servo
+  delay(30);
+  clawservo.write(0); //open claw
+  delay(30);
+  reachservo.write(130); //raise the arm high to avoid hitting objects
+  delay(40);
+  liftservo.write(60); // raise the lift servo
   delay(30);
 }
 void moveGreen()
 {
-  liftservo.write(30);
+  reachservo.write(130); // raise reach servo
+  delay(40);
+  liftservo.write(60); // raise lift servo
   delay(30);
-  baseservo.write(115);
+  baseservo.write(115); // move to green zone
+  delay(30);
+  reachservo.write(90); // lower to far distance
+  delay(30);
+  liftservo.write(100); //lower the lift servo
+  delay(30);
+  clawservo.write(0); //open claw
+  delay(30);
+  reachservo.write(130); //raise the arm high to avoid hitting objects
+  delay(40);
+  liftservo.write(60); // raise the lift servo
   delay(30);
 }
+
+int findRed()
+{
+  // Setting red filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  // Reading the output frequency
+  frequency = pulseIn(sensorOut, LOW);
+  // Printing the value on the serial monitor
+  Serial.print("R= ");//printing name
+  Serial.print(frequency);//printing RED color frequency
+  Serial.print("  ");
+  delay(100);
+  return frequency;
+}
+int findBlue()
+{
+  // Setting Blue filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  // Reading the output frequency
+  frequency = pulseIn(sensorOut, LOW);
+  // Printing the value on the serial monitor
+  Serial.print("B= ");//printing name
+  Serial.print(frequency);//printing RED color frequency
+  Serial.println("  ");
+  delay(100);
+  return frequency;
+}
+
+int findGreen()
+{
+  // Setting Green filtered photodiodes to be read
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  // Reading the output frequency
+  frequency = pulseIn(sensorOut, LOW);
+  // Printing the value on the serial monitor
+  Serial.print("G= ");//printing name
+  Serial.print(frequency);//printing RED color frequency
+  Serial.print("  ");
+  delay(100);
+  return frequency;
+}
+
