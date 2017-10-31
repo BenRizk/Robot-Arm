a26c3a4f5faf53686cf698b3ba5b83b9dfc03c6d
Eric Taylor <tayloreric@rogers.com>
2017-10-31 01:27:40 -0400

    Fix formatting

--- Automation_of_Arm.ino
+++ Automation_of_Arm.ino
@@ -8,58 +8,52 @@
 */
 
 #include <Servo.h>
-
-Servo baseservo;  // create servo object to control a servo
+Servo baseservo;    // Create servo object to control a servo
 Servo reachservo;
 Servo liftservo;
 Servo clawservo;
 
-int pos = 0;    // variable to store the servo position
+int pos = 0;        // Variable to store the servo position
 
-int trigPin = 10;     // variables for ultrasonic sensor pin num
+int trigPin = 10;   // Variables for ultrasonic sensor pin num
 int echoPin = 13;
   
 void setup() {
-  baseservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  reachservo.attach(3); // attach the servo for reaching to pin 3
-  liftservo.attach(5); // attach the servo for lifting to pin 5
-  clawservo.attach(6); // attach the servo for the claw to pin 6
+  baseservo.attach(9);    // Attaches the servo on pin 9 to the servo object
+  reachservo.attach(3);   // Attach the servo for reaching to pin 3
+  liftservo.attach(5);    // Attach the servo for lifting to pin 5
+  clawservo.attach(6);    // Attach the servo for the claw to pin 6
 
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin, OUTPUT);   // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);    // Sets the echoPin as an Input
   Serial.begin(9600);
-  
 }
 
 void loop() {
-  
-
-sweep(pos, trigPin, echoPin);
-
+  sweep(pos, trigPin, echoPin);
 }
 
 void sweep(int pos,int trigPin, int echoPin)
 {
    int distance;
-   for (pos = 0; pos <= 180; pos++) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
+   for (pos = 0; pos <= 180; pos++) {  // Goes from 0 degrees to 180 degrees in steps of 1 degree
     baseservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(15);                         // waits 15ms for the servo to reach the position
     distance = distanceSensor(trigPin, echoPin);
-      clawservo.write(180);
-       delay(15);
+    clawservo.write(180);
+    delay(15);
     if(distance > 15 && distance < 20)
     {
       reachOut();
     }
   }
-  for (pos = 180; pos >= 0; pos--) { // goes from 180 degrees to 0 degrees
-    baseservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-   distance = distanceSensor(trigPin, echoPin);
-     clawservo.write(180);
-     delay(15);
-   if(distance > 15 && distance < 20)
+  for (pos = 180; pos >= 0; pos--) {   // Goes from 180 degrees to 0 degrees
+    baseservo.write(pos);              // Tell servo to go to position in variable 'pos'
+    delay(15);                         // Waits 15ms for the servo to reach the position
+    distance = distanceSensor(trigPin, echoPin);
+    clawservo.write(180);
+    delay(15);
+    if(distance > 15 && distance < 20)
     {
       reachOut();
     }
@@ -68,7 +62,6 @@ void sweep(int pos,int trigPin, int echoPin)
 
 int distanceSensor(int trigPin, int echoPin)
 {
-
   long duration;
   int distance;
   // Clears the trigPin
@@ -90,35 +83,32 @@ int distanceSensor(int trigPin, int echoPin)
 
 void reachOut()
 {
-//extend the arm
+// Extend the arm
  for(int i = 0; i < 90; i++)
  {
-  reachservo.write(i);
-  delay(15);
+   reachservo.write(i);
+   delay(15);
  }
-  for(int i = 0; i < 90; i++)
+ for(int i = 0; i < 90; i++)
  {
-  liftservo.write(i);
-  delay(15);
+   liftservo.write(i);
+   delay(15);
  }
-//close the claw
-  clawservo.write(0);
-  delay(15);
-  clawservo.write(180);
-  delay(15);
-  
- //retract arm
-  for(int i = 89; i > 35; i--)
+ // Close the claw
+ clawservo.write(0);
+ delay(15);
+ clawservo.write(180);
+ delay(15); 
+ // Retract arm
+ for(int i = 89; i > 35; i--)
  {
-  reachservo.write(i);
-  delay(15);
+   reachservo.write(i);
+   delay(15);
  }
-  for(int i = 89; i > 35; i--)
+ for(int i = 89; i > 35; i--)
  {
-  liftservo.write(i);
-  delay(15);
+   liftservo.write(i);
+   delay(15);
  }
-  delay(15);
-  
+ delay(15); 
 }
-
