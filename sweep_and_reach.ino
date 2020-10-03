148ac04578ade7c616844de1e313a394404f0eef
Eric Taylor <tayloreric@rogers.com>
2020-10-02 22:02:32 -0400

    Improve readability and documentation

--- sweep_and_reach.ino
+++ sweep_and_reach.ino
@@ -7,48 +7,44 @@
 */
 
 #include <Servo.h>
-
-Servo baseservo;  // create servo object to control a servo
+Servo baseservo;     // Create servo object to control a servo
 Servo reachservo;
 Servo liftservo;
 Servo clawservo;
 
-int pos = 0;    // variable to store the servo position
+int pos = 0;          // Variable to store the servo position
 
-int trigPin = 10;     // variables for ultrasonic sensor pin num
+int trigPin = 10;     // Variables for ultrasonic sensor pin num
 int echoPin = 13;
   
 void setup() {
-  baseservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  reachservo.attach(3); // attach the servo for reaching to pin 3
-  liftservo.attach(5); // attach the servo for lifting to pin 5
-  clawservo.attach(6); // attach the servo for the claw to pin 6
+  baseservo.attach(9);    // Attaches the servo on pin 9 to the servo object
+  reachservo.attach(3);   // Attach the servo for reaching to pin 3
+  liftservo.attach(5);    // Attach the servo for lifting to pin 5
+  clawservo.attach(6);    // Attach the servo for the claw to pin 6
 
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
   Serial.begin(9600);
-  
 }
 
 void loop() {
-//beginning position
+// Beginning position
 liftservo.write(80);
 reachservo.write(50);
 
 sweep(pos, trigPin, echoPin);
-
 }
 
 void sweep(int pos,int trigPin, int echoPin)
 {
    int distance;
-   for (pos = 0; pos <= 80; pos++) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    baseservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+   for (pos = 0; pos <= 80; pos++) {   // Goes from 0 degrees to 180 degrees in steps of 1 degree
+    baseservo.write(pos);              // Tell servo to go to position in variable 'pos'
+    delay(15);                         // Waits 15ms for the servo to reach the position
     distance = distanceSensor(trigPin, echoPin);
       clawservo.write(180);
-       delay(15);
+      delay(15);
     if(distance > 15 && distance < 20)
     {
       reachOut();
@@ -56,9 +52,9 @@ void sweep(int pos,int trigPin, int echoPin)
       liftservo.write(80);
     }
   }
-  for (pos = 79; pos >= 0; pos--) { // goes from 180 degrees to 0 degrees
-    baseservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 79; pos >= 0; pos--) {    // Goes from 180 degrees to 0 degrees
+    baseservo.write(pos);              // Tell servo to go to position in variable 'pos'
+    delay(15);                         // Waits 15ms for the servo to reach the position
    distance = distanceSensor(trigPin, echoPin);
      clawservo.write(180);
      delay(15);
@@ -94,52 +90,23 @@ int distanceSensor(int trigPin, int echoPin)
 
 void reachOut()
   {
-
-  clawservo.write(35); //open claw
+  clawservo.write(35);  // Open claw
   delay(300);
-
-   //extend the arm
-  
-    for(int i = 0; i < 90; i++)
+  // Extend the arm
+  for(int i = 0; i < 90; i++)
+  {
+    reachservo.write(i);
+    delay(30);
+  }
+  clawservo.write(180); // Close claw
+  delay(300);
+  // Retract arm
+  for(int i = 89; i > 35; i--)
   {
     reachservo.write(i);
     delay(30);
   }
  
-   clawservo.write(180); //close claw
-    delay(300);
-  
-  //retract arm
-    for(int i = 89; i > 35; i--)
-   {
-    reachservo.write(i);
-    delay(30);
-  }
-   
-//trying to make the arm stop before it reaches its object
-/*
-int a = 0;
-int distance; 
-   
-  do{
-    distance = distanceSensor(trigPin, echoPin);
-    Serial.print("a:");
-    Serial.print(a);
-    a++;
-    reachservo.write(a);
-    delay(300);
-  }while(distance < 4);
-
-  clawservo.write(180); //close claw
-    delay(300);
-
-     for(int i = a; i > 35; i--)
-   {
-    reachservo.write(i);
-    delay(30);
-  }
-*/
-
 }
 void moveBlue()
 {
@@ -162,4 +129,3 @@ void moveGreen()
   baseservo.write(115);
   delay(30);
 }
-
