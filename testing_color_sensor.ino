63270e4f2723a5eb718df7d001390545072fffc5
Eric Taylor <tayloreric@rogers.com>
2020-10-02 22:01:24 -0400

    Improve readability and documentation

--- testing_color_sensor.ino
+++ testing_color_sensor.ino
@@ -7,34 +7,34 @@
 */
 
 #include <Servo.h>
-//servos
-Servo baseservo;  // create servo object to control a servo
+// Servos
+Servo baseservo;  // Create servo object to control a servo
 Servo reachservo;
 Servo liftservo;
 Servo clawservo;
-//color sensor
+//Color sensor
 #define S0 4
 #define S1 7
 #define S2 11
 #define S3 12
 #define sensorOut 8
 
-int pos = 0;    // variable to store the servo position
+int pos = 0;    // Variable to store the servo position
 
-int trigPin = 10;     // variables for ultrasonic sensor pin num
+int trigPin = 10;     // Variables for ultrasonic sensor pin num
 int echoPin = 13;
   
 void setup() {
-  baseservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  reachservo.attach(3); // attach the servo for reaching to pin 3
-  liftservo.attach(5); // attach the servo for lifting to pin 5
-  clawservo.attach(6); // attach the servo for the claw to pin 6
+  baseservo.attach(9);  // Attaches the servo on pin 9 to the servo object
+  reachservo.attach(3); // Attach the servo for reaching to pin 3
+  liftservo.attach(5);  // Attach the servo for lifting to pin 5
+  clawservo.attach(6);  // Attach the servo for the claw to pin 6
 
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
   Serial.begin(9600);
-  // needed for color sensor
-  // we need to assign s0, s1, s2, s3, and sensorout to digital port
+  // Needed for color sensor
+  // We need to assign s0, s1, s2, s3, and sensorout to digital port
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
@@ -49,68 +49,66 @@ void setup() {
 }
 
 void loop() {
-//beginning position
+// Beginning position
 liftservo.write(85);  // 85 o.g
 reachservo.write(40);  // 40 o.g
 
 sweep(pos, trigPin, echoPin);
-
 }
 
 void sweep(int pos,int trigPin, int echoPin)
 {
-   int distance; // int for knowing distance of objects
-   int redValue;  // int for knowing the value for red 
-   int blueValue; // int for knowing the value for blue 
-   int greenValue; // int for knowing the value for green
-   for (pos = 180; pos >= 140; pos--) { // goes from 180 degrees to 140 degrees
-    // in steps of 1 degree
-    baseservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-    distance = distanceSensor(trigPin, echoPin); //find object distance from senor
-      clawservo.write(180); // make sure the claw is closed
-       delay(15);
+   int distance;   // Int for knowing distance of objects
+   int redValue;   // Int for knowing the value for red 
+   int blueValue;  // Int for knowing the value for blue 
+   int greenValue; // Int for knowing the value for green
+   for (pos = 180; pos >= 140; pos--) { // Goes from 180 degrees to 140 degrees in steps of 1 degree
+    baseservo.write(pos);               // Tell servo to go to position in variable 'pos'
+    delay(15);                          // Waits 15ms for the servo to reach the position
+    distance = distanceSensor(trigPin, echoPin); // Find object distance from senor
+      clawservo.write(180); // Make sure the claw is closed
+      delay(15);
     if(distance > 7 && distance < 20)
     {
-      //reach out to grab object
+      // Reach out to grab object
       reachOut(trigPin,echoPin);
       delay(40);
-      //make sure the arm is in original position
+      // Make sure the arm is in original position
       reachservo.write(50);
       delay(30);
       liftservo.write(80);
       delay(30);
-      // begin process of organizing by color
-      //find the color values
+      // Begin process of organizing by color
+      // Find the color values
       redValue = findRed();
       blueValue = findBlue();
       greenValue = findGreen();
-      //move object to appropriate area
+      // Move object to appropriate area
       moveBlock(redValue, blueValue, greenValue, pos);
     }
   }
-  for (pos = 139; pos <= 180; pos++) { // goes from 180 degrees to 0 degrees
-    baseservo.write(pos);              // tell servo to go to position in variable 'pos'//
-    delay(15);                       // waits 15ms for the servo to reach the position
-    distance = distanceSensor(trigPin, echoPin); //find object distance from senor
-     clawservo.write(180); // make sure the claw is closed
-     delay(15);
-   if(distance > 7 && distance < 20)
+  for (pos = 139; pos <= 180; pos++) { // Goes from 180 degrees to 0 degrees
+    baseservo.write(pos);              // Tell servo to go to position in variable 'pos'//
+    delay(15);                         // Waits 15ms for the servo to reach the position
+    distance = distanceSensor(trigPin, echoPin); // Find object distance from senor
+    clawservo.write(180); // Make sure the claw is closed
+    delay(15);
+    if(distance > 7 && distance < 20)
     {
-      //reach out to grab object
+      // Reach out to grab object
       reachOut(trigPin,echoPin);
       delay(40);
-       //make sure the arm is in original position
+      // Make sure the arm is in original position
       reachservo.write(50);
       delay(30);
       liftservo.write(80);
       delay(30);
-      // begin process of organizing by color
-      //find the color values
+      // Begin process of organizing by color
+      // Find the color values
       redValue = findRed();
       blueValue = findBlue();
       greenValue = findGreen();
-      //move object to appropriate area
+      // Move object to appropriate area
       moveBlock(redValue, blueValue, greenValue, pos);
     }
   }
@@ -139,40 +137,25 @@ int distanceSensor(int trigPin, int echoPin)
 
 void reachOut(int trigPin,int echoPin)
   {
-  //int a; // this integer is for knowing how close the head is to an object
-  //int b; // this holds the angle the arm was last extended to
   clawservo.write(35); //open claw
   delay(300);
-
-   //extend the arm
+  // Extend the arm
     for(int i = 0; i < 110; i++)
-  {
-    /* code for knowing how close sensor is to object
-    a = distanceSensor(trigPin, echoPin);
-    Serial.print("a:");
-    Serial.print(a);
-    */
+    {
       reachservo.write(i);
-      if(i > 60)  //only use liftservo if reach servo doesn't reach object alone
+      if(i > 60)  // Only use liftservo if reach servo doesn't reach object alone
       {
         liftservo.write(i+20);
         delay(30);
       }
       delay(30);
-    /* code for knowing how close sensor is to object
-    if(a <= 1)  //end for loop when the head is close enough
-    {
-      b = i; // hold the last angle the arm was at
-      break; //for the loop to end
-    }
-    */
   }
 
-   clawservo.write(180); //close claw
-    delay(300);
+   clawservo.write(180); // Close claw
+   delay(300);
   
-  //retract arm
-    for(int i = 110; i > 35; i--)
+   // Retract arm
+   for(int i = 110; i > 35; i--)
    {
     reachservo.write(i);
     delay(30);
@@ -186,68 +169,68 @@ void reachOut(int trigPin,int echoPin)
 }
 void moveBlue(int basePos)
 {
-  reachservo.write(130); //raise the arm high to avoid hitting objects
+  reachservo.write(130); // Raise the arm high to avoid hitting objects
   delay(100);
-  liftservo.write(60); // raise the lift servo
+  liftservo.write(60);   // Raise the lift servo
   delay(100);
-  for(int i = basePos; i > 70; i--) // make the base servo move slower
+  for(int i = basePos; i > 70; i--) // Make the base servo move slower
   { 
-  baseservo.write(i); // move to blue zone
+  baseservo.write(i);    // Move to blue zone
   delay(40);
   }
-  reachservo.write(90); // lower to far distance
+  reachservo.write(90);  // Lower to far distance
   delay(100);
-  liftservo.write(100); //lower the lift servo
+  liftservo.write(100);  //Lower the lift servo
   delay(100);
-  clawservo.write(35); //open claw
+  clawservo.write(35);   // Open claw
   delay(100);
-  reachservo.write(130); //raise the arm high to avoid hitting objects
+  reachservo.write(130); // Raise the arm high to avoid hitting objects
   delay(100);
-  liftservo.write(60); // raise the lift servo
+  liftservo.write(60);   // Raise the lift servo
   delay(100);
 }
 void moveRed(int basePos)
 {
-  reachservo.write(130); // extend reach servo
+  reachservo.write(130); // Extend reach servo
   delay(100);
-  liftservo.write(60); // raise lift servo
+  liftservo.write(60);   // Raise lift servo
   delay(100);
- for(int i = basePos; i > 40; i--) // make the base servo move slower
+  for(int i = basePos; i > 40; i--) // Make the base servo move slower
   { 
-  baseservo.write(i); // move to red zone
+  baseservo.write(i); // Move to red zone
   delay(40);
   }
-  reachservo.write(90); // lower to far distance
+  reachservo.write(90);   // Lower to far distance
   delay(100);
-  liftservo.write(100); //lower the lift servo
+  liftservo.write(100);   // Lower the lift servo
   delay(100);
-  clawservo.write(35); //open claw
+  clawservo.write(35);    // Open claw
   delay(100);
-  reachservo.write(130); //raise the arm high to avoid hitting objects
+  reachservo.write(130);  // Raise the arm high to avoid hitting objects
   delay(100);
-  liftservo.write(60); // raise the lift servo
+  liftservo.write(60);    // Raise the lift servo
   delay(100);
 }
 void moveGreen(int basePos)
 {
-  reachservo.write(130); // raise reach servo
+  reachservo.write(130); // Raise reach servo
   delay(100);
-  liftservo.write(60); // raise lift servo
+  liftservo.write(60); // Raise lift servo
   delay(100);
-  for(int i = basePos; i > 115; i--) // make the base servo move slow 
+  for(int i = basePos; i > 115; i--) // Make the base servo move slow 
   { 
-  baseservo.write(i); // move to green zone
+  baseservo.write(i);     // Move to green zone
   delay(40);
   }
-  reachservo.write(90); // lower to far distance
+  reachservo.write(90);   // Lower to far distance
   delay(100);
-  liftservo.write(100); //lower the lift servo
+  liftservo.write(100);   // Lower the lift servo
   delay(100);
-  clawservo.write(35); //open claw
+  clawservo.write(35);    // Open claw
   delay(100);
-  reachservo.write(130); //raise the arm high to avoid hitting objects
+  reachservo.write(130);  // Raise the arm high to avoid hitting objects
   delay(100);
-  liftservo.write(60); // raise the lift servo
+  liftservo.write(60);    // Raise the lift servo
   delay(100);
 }
 
@@ -259,8 +242,8 @@ int findRed()
   // Reading the output frequency
   int frequency = pulseIn(sensorOut, LOW);
   // Printing the value on the serial monitor
-  Serial.print("R= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
+  Serial.print("R= ");       // Printing name
+  Serial.print(frequency);   // Printing RED color frequency
   Serial.print("  ");
   delay(100);
   return frequency;
@@ -273,8 +256,8 @@ int findBlue()
   // Reading the output frequency
   int frequency = pulseIn(sensorOut, LOW);
   // Printing the value on the serial monitor
-  Serial.print("B= ");//printing name
-  Serial.print(frequency);//printing BLUE color frequency
+  Serial.print("B= ");       // Printing name
+  Serial.print(frequency);   // Printing BLUE color frequency
   Serial.println("  ");
   delay(100);
   return frequency;
@@ -288,8 +271,8 @@ int findGreen()
   // Reading the output frequency
   int frequency = pulseIn(sensorOut, LOW);
   // Printing the value on the serial monitor
-  Serial.print("G= ");//printing name
-  Serial.print(frequency);//printing GREEN color frequency
+  Serial.print("G= ");       // Printing name
+  Serial.print(frequency);   // Printing GREEN color frequency
   Serial.print("  ");
   delay(100);
   return frequency;
@@ -297,27 +280,19 @@ int findGreen()
 
 void moveBlock(int redValue,int blueValue,int greenValue,int basePos)
 {
-  //object is red when red value is least
+      // Object is red when red value is least
       if(redValue < blueValue && redValue < greenValue)
       {
         moveRed(basePos);
       }
-      //object is blue if blue value is least
+      // Object is blue if blue value is least
       if(blueValue < redValue && blueValue < greenValue)
       {
         moveBlue(basePos);
       }
-      // object is green if green value is least
+      // Object is green if green value is least
       if(greenValue < blueValue && greenValue < redValue)
       {
         moveGreen(basePos);
       }
-      //object is black if all values are greater than 100
-      /*
-      if(redValue > 100 && blueValue > 100 && greenValue > 100)
-      {
-        //not made yet
-      }
-    */
 }
-
